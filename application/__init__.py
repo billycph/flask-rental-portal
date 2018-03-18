fa178de472070012e579d7d0d36a19223eba3e21
peihao.chen <chenpe@us.ibm.com>
2018-03-18 11:20:18 -0400

    changing file structure for gunicorn

--- application/__init__.py
+++ application/__init__.py
@@ -9,11 +9,11 @@ logger.setLevel(logging.INFO)
 from application import views
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
-import SQLAlchemy_declear
+from dabase import views as db_declear
 
 engine = create_engine('sqlite:///argo_food.db')
 try:
     engine.connect()
     engine.execute('SELECT 1;')
 except OperationalError:
-    SQLAlchemy_declear()
+    db_declear()
